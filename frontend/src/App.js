b6cda0236c44058f08eddfa35499a2af7e65f42a
kmcandersen <kristen.andersen@gmail.com>
2021-02-05 16:40:24 -0600

    add nav & footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
     <>
-      <h1>welcome to perennial candidates</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1 className='text-center'>welcome</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import '../index.css';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar expand='md' fixed='top' className='px-5 py-3'>
+        <Navbar.Brand href='/'>perennial candidates</Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ml-auto'>
+            <Nav.Link href='/search'>
+              <i className='fas fa-search pr-2'></i>SEARCH
+            </Nav.Link>
+            <Nav.Link href='/signin'>
+              <i className='fas fa-user pr-2'></i>SIGN IN
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
