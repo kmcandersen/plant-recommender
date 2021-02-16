299fc990727ddd9219cdc36db0268fa2bca4cfe1
kmcandersen <kristen.andersen@gmail.com>
2021-02-15 19:22:43 -0600

    add loading spinner & error alert message

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import CardPopular from '../components/CardPopular';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { listPlants } from '../actions/plantActions';
 
 const HomeScreen = () => {
-  const [plants, setPlants] = useState([]);
+  const dispatch = useDispatch();
+  const plantList = useSelector((state) => state.plantList);
+  const { loading, error, plants } = plantList;
 
   useEffect(() => {
-    const fetchPlants = async () => {
-      const { data } = await axios.get('/api/plants');
-      setPlants(data);
-    };
-    fetchPlants();
-  }, []);
+    dispatch(listPlants());
+  }, [dispatch]);
+
   return (
     <>
       <h2>Most Popular</h2>
-      <Row className='my-5'>
-        {plants.map((plant) => (
-          <Col xs={12} md={6} lg={3} key={plant._id} className='mb-4'>
-            <CardPopular plant={plant} />
-          </Col>
-        ))}
-      </Row>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row className='my-5'>
+          {plants.map((plant) => (
+            <Col xs={12} md={6} lg={3} key={plant._id} className='mb-4'>
+              <CardPopular plant={plant} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
